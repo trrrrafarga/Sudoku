75e5e9153adfdc7aa9714abc8eb5104ece6c7b53
trrrrafarga <34438376+trrrrafarga@users.noreply.github.com>
2018-12-04 20:32:42 +0800

    Add files via upload
    今天写了一个bug,删光了,打算重构,真好.

---
+++ sudoku.cpp
@@ -0,0 +1,33 @@
+#include "iostream"
+#include "algorithm"
+#include "fstream"
+using namespace std;
+string path = "./sudoku.txt";
+bool Judge()
+{
+}
+bool build_map()
+{
+} 
+void print()
+{
+}
+int main(int argc, char const *argv[])
+{
+	int N,i;
+	for (int i = 1; i < 10; ++i)
+	{
+		num.insert(i);
+	}
+	scanf("%d",&N);
+	int theFirstPoint = (2 + 5)% 9 + 1;
+	if (N > 0)
+	{
+		
+	}
+	else
+	{
+
+	}
+	return 0;
+}
